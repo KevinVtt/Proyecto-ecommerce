c2e5ca663a77fe76f136b3166e622baf0c5feb7f
kevinvittor <kevinvittor579@gmail.com>
2025-03-17 16:32:46 -0300

    Test hasta el momento con JpaRepository

--- src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IProductoCantidad.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IProductoCantidad.java
@@ -1,9 +1,9 @@
 package com.kevn.project.ecommerce.e_commerce.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kevn.project.ecommerce.e_commerce.models.ProductoCantidad;
 
-public interface IProductoCantidad extends CrudRepository<ProductoCantidad,Long>{
+public interface IProductoCantidad extends JpaRepository<ProductoCantidad,Long>{
 
 }

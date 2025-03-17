e3cd68e521759c844c2c97932d6f389e5e4c4dd2
kevinvittor <kevinvittor579@gmail.com>
2025-03-17 16:30:56 -0300

    add fetchType.EAGER y Cascade.Type.ALL en la relacion ManyToOnede itemProducto

--- src/main/java/com/kevn/project/ecommerce/e_commerce/models/ProductoCantidad.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/models/ProductoCantidad.java
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+
+/*
+ * No utilizar el producto cantidad para eliminar.
+ * Ya que si lo utilizamos eliminariamos item_producto y el pedido.
+ */
 @Entity
 @Table(name = "producto_cantidad")
 @Getter
@@ -19,11 +24,10 @@ public class ProductoCantidad {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "item_producto_id", nullable = false)
     @JsonIgnore
     private ItemProducto itemProducto;
-
     @ManyToOne
     @JoinColumn(name = "producto_id", nullable = true)
     private Producto producto;

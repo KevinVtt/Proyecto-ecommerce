7ebb96e8d98f0bc8bf46c406ace8b70fea7300b4
kevinvittor <kevinvittor579@gmail.com>
2025-03-17 21:10:35 -0300

    delete query personalizada

--- src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IPedido.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IPedido.java
@@ -1,17 +1,8 @@
 package com.kevn.project.ecommerce.e_commerce.repositories;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.kevn.project.ecommerce.e_commerce.models.Pedido;
 
 public interface IPedido extends CrudRepository<Pedido,Long> {
-
-@Query("SELECT p FROM Pedido p WHERE NOT EXISTS " +
-       "(SELECT 1 FROM ItemProducto i WHERE i.usuario.id = :usuarioId AND i.id = p.itemProducto.id)")
-List<Pedido> findPedidosSinCoincidenciasConCarritoYUsuario(@Param("usuarioId") Long usuarioId);
-
 }

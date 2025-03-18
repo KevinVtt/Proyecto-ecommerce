b6a01815fcbc0d472db8e112bcfb30ab643bbdf5
kevinvittor <kevinvittor579@gmail.com>
2025-03-17 21:06:53 -0300

    change OneToOne itemProducto a OneToMany

--- src/main/java/com/kevn/project/ecommerce/e_commerce/models/Pedido.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/models/Pedido.java
@@ -3,12 +3,14 @@ package com.kevn.project.ecommerce.e_commerce.models;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -30,15 +32,12 @@ public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    @JoinColumn(name = "item_producto_id",nullable = false)
-    private ItemProducto itemProducto;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<ItemProducto> itemProducto;
     
     private LocalDateTime fecha = LocalDateTime.now();
     private String estado;
-
-    public boolean carritoExistInPedido(List<Pedido> list){
-        return list.stream().anyMatch(p -> (p.getItemProducto().equals(this.getItemProducto())));
-    }
 
 }

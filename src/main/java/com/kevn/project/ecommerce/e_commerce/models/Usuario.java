36f1f8449620214280cf805ef1e7601ff9e919e4
kevinvittor <kevinvittor579@gmail.com>
2025-03-17 21:04:21 -0300

    change OneToMany itemProducto a OneToOne, simulando un carrito de compras

--- src/main/java/com/kevn/project/ecommerce/e_commerce/models/Usuario.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/models/Usuario.java
@@ -1,15 +1,11 @@
 package com.kevn.project.ecommerce.e_commerce.models;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -34,9 +30,8 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL,orphanRemoval = true)
-    @JsonIgnore
-    private List<ItemProducto> itemProducto;
+    @OneToOne(mappedBy = "usuario",cascade = CascadeType.ALL,orphanRemoval = true)
+    private ItemProducto itemProducto;
     
     private String nombre;
     private String email;

34a735f4a485bc9c2a0dc3424dacadceda97ee34
kevinvittor <kevinvittor579@gmail.com>
2025-03-18 16:16:55 -0300

    quite el import de CrudRepository

--- src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IPedido.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IPedido.java
@@ -1,7 +1,6 @@
 package com.kevn.project.ecommerce.e_commerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.kevn.project.ecommerce.e_commerce.models.Pedido;
 
--- src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IProducto.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/repositories/IProducto.java
@@ -1,10 +1,10 @@
 package com.kevn.project.ecommerce.e_commerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.kevn.project.ecommerce.e_commerce.models.Producto;
 
 public interface IProducto extends JpaRepository<Producto,Long> {
     boolean existsByNombre(String nombre);
+    
 }

fd0bdf7efc482931f493a9fdb3c4992b153f3a1a
jordan albano <jalbano@unrn.edu.ar>
2025-03-18 09:45:43 -0300

    Cambiar el nombre de HandLerExceptionController y actualizar mensajes de excepción

--- src/main/java/com/kevn/project/ecommerce/e_commerce/controllers/HanlderExceptionController.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/controllers/HanlderExceptionController.java
@@ -1,9 +1,12 @@
-package com.kevn.project.ecommerce.e_commerce.exception;
+package com.kevn.project.ecommerce.e_commerce.controllers;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.kevn.project.ecommerce.e_commerce.exception.AlreadyExistException;
+import com.kevn.project.ecommerce.e_commerce.exception.NotExistException;
+import com.kevn.project.ecommerce.e_commerce.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +17,6 @@ public class HanlderExceptionController {
 
     @ExceptionHandler({NotFoundException.class})
     public ResponseEntity<?> NotFound(NotFoundException e){
-
         Map<String,Object> error = new HashMap<>();
         error.put("Date", new Date().toString());
         error.put("Message", e.getMessage());
@@ -25,7 +27,6 @@ public class HanlderExceptionController {
 
     @ExceptionHandler({AlreadyExistException.class})
     public ResponseEntity<?> AlreadyExist(AlreadyExistException e){
-
         Map<String,Object> error = new HashMap<>();
         error.put("Date", new Date().toString());
         error.put("Message", e.getMessage());
@@ -36,11 +37,10 @@ public class HanlderExceptionController {
 
     @ExceptionHandler({NullPointerException.class})
     public ResponseEntity<?> NullPonter(NullPointerException e){
-
         Map<String,Object> error = new HashMap<>();
         error.put("Date", new Date().toString());
         error.put("Message", e.getMessage());
-        error.put("Error", "El objeto que estas intentando mandar es nulo");
+        error.put("Error", "El objeto no puede ser nulo");
         error.put("Cause", e.getCause());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }

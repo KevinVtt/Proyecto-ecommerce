16aa216abb339b13cad7fa69f3ae7ac1c2f98ee0
kevinvittor <kevinvittor579@gmail.com>
2025-03-17 21:16:03 -0300

    change en findall y simulando compra de productos

--- src/main/java/com/kevn/project/ecommerce/e_commerce/controllers/ItemProductoController.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/controllers/ItemProductoController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.kevn.project.ecommerce.e_commerce.exception.AlreadyExistException;
+import com.kevn.project.ecommerce.e_commerce.exception.NotExistException;
+import com.kevn.project.ecommerce.e_commerce.exception.NotFoundException;
 import com.kevn.project.ecommerce.e_commerce.models.ItemProducto;
 import com.kevn.project.ecommerce.e_commerce.services.ItemProductoService;
 
@@ -16,7 +20,7 @@ public class ItemProductoController {
     @Autowired
     private ItemProductoService service;
 
-    @GetMapping("/findall")
+    @GetMapping("/all")
     public ResponseEntity<?> findAll() {
         if (service.findAll().isEmpty()) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -49,6 +53,52 @@ public class ItemProductoController {
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.ok("ItemProducto eliminado!");
+    }
+
+    /*
+     * Este metodo sera de prueba, lo que hara es simular una compra.
+     * el id debe ser del ItemProducto
+     * Genera un pedido si es nulo.
+     */
+    @PutMapping("/comprar/{id}")
+    public ResponseEntity<?> comprar(@PathVariable Long id){
+        ItemProducto itDb = service.findById(id);
+        if(itDb != null){
+            if(itDb.getProductos().isEmpty()){
+                throw new NotExistException("Los productos no existen");
+            }else{
+                if(itDb.getPedido() == null){
+                    service.save(itDb);
+                    return ResponseEntity.ok("La compra ha sido completada!");
+                }else{
+                    throw new AlreadyExistException("Ya existe un pedido para esta compra, debe esperar al repartidor");
+                }
+            }
+        }else{
+            throw new NotFoundException("El objeto que estas buscando no existe en la base de datos");
+        }
+    }
+
+    /*
+     * Realizar otra compra !
+     * Este metodo es de forma de prueba para verificar si funciona, la idea ahora es que si quiero hacer otra compra los productos se eliminen.
+     * Elimina la lista de productos y setea en nulo el pedido
+     */
+
+    @PutMapping("/otra-compra/{id}")
+    public ResponseEntity<?> comprarDenuevo(@PathVariable Long id){
+        ItemProducto itDb = service.findById(id);
+        if(itDb != null){
+
+            if(itDb.getProductos().isEmpty()){
+                throw new RuntimeException("Tu lista esta vacia, y esta lista para agregar productos");
+            }else{
+                service.eliminarTodosLosProductos(itDb.getId());
+                return ResponseEntity.ok("Tu lista se ha reestablecido de nuevo, para la siguiente compra");
+            }
+        }else{
+            throw new NotFoundException("El item producto no se ha encontrado para realizar la compra nuevamente");
+        }
     }
 
     /* Lógica de negocio para los productos de ItemProducto */

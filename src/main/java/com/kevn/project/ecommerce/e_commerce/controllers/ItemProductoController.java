8e6348dc9c66725848c47e5d1d9cec0b195b8f18
kevinvittor <kevinvittor579@gmail.com>
2025-03-24 02:07:46 -0300

    change class name, before productoCantidad after itemProducto

--- src/main/java/com/kevn/project/ecommerce/e_commerce/controllers/ItemProductoController.java
+++ src/main/java/com/kevn/project/ecommerce/e_commerce/controllers/ItemProductoController.java
@@ -1,11 +1,11 @@
 package com.kevn.project.ecommerce.e_commerce.controllers;
 
-import java.util.Collections;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.kevn.project.ecommerce.e_commerce.exception.NotFoundException;
 import com.kevn.project.ecommerce.e_commerce.models.ItemProducto;
 import com.kevn.project.ecommerce.e_commerce.services.ItemProductoService;
 
@@ -13,18 +13,11 @@ import com.kevn.project.ecommerce.e_commerce.services.ItemProductoService;
 @RequestMapping("/api/item-producto")
 public class ItemProductoController {
 
-    private final ItemProductoService service;
-
-    public ItemProductoController(ItemProductoService service) {
-        this.service = service;
-    }
+    @Autowired
+    private ItemProductoService service;
 
     @GetMapping("/all")
     public ResponseEntity<?> findAll() {
-        if (service.findAll().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Collections.singletonMap("Error", "La lista está vacía"));
-        }
         return ResponseEntity.ok(service.findAll());
     }
 
@@ -34,97 +27,23 @@ public class ItemProductoController {
     }
 
     @PostMapping("/insert")
-    public ResponseEntity<?> insert(@RequestBody ItemProducto itemProducto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(itemProducto));
+    public ResponseEntity<?> insert(@RequestBody ItemProducto productoCantidad) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(productoCantidad));
     }
 
     @PostMapping("/insertAll")
-    public ResponseEntity<?> insertMultiple(@RequestBody List<ItemProducto> itemProductos) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveAll(itemProductos));
+    public ResponseEntity<?> insertMultiple(@RequestBody List<ItemProducto> productosCantidad) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveAll(productosCantidad));
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> update(@RequestBody ItemProducto itemProducto) {
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.save(itemProducto));
+    public ResponseEntity<?> update(@RequestBody ItemProducto productoCantidad) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.save(productoCantidad));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
-        return ResponseEntity.ok("ItemProducto eliminado!");
-    }
-
-    /*
-     * Este metodo sera de prueba, lo que hara es simular una compra.
-     * el id debe ser del ItemProducto
-     * Genera un pedido si es nulo.
-     */
-    @PutMapping("/comprar/{id}")
-    public ResponseEntity<?> comprar(@PathVariable Long id) {
-
-        ItemProducto itDb = service.findById(id);
-
-        if (itDb.getId() != null) {
-            if (!service.verificarSiExistenProductos(itDb)) throw new NotFoundException("No existen los productos en tu cuenta");
-            service.updateEstadoPedido(itDb);
-            return ResponseEntity.ok("La compra ha sido completada!");
-        }
-        
-        throw new NotFoundException("El objeto que estas buscando no existe en la base de datos");
-        
-    }
-    /*
-     * Realizar otra compra !
-     * Este metodo es de forma de prueba para verificar si funciona, la idea ahora
-     * es que si quiero hacer otra compra los productos se eliminen.
-     * Elimina la lista de productos y setea en nulo el pedido
-     */
-
-    @PutMapping("/otra-compra/{id}")
-    public ResponseEntity<?> comprarDenuevo(@PathVariable Long id) {
-        ItemProducto itDb = service.findById(id);
-        if (itDb != null) {
-
-            if (!service.verificarSiExistenProductos(itDb)) {
-                throw new RuntimeException("Tu lista esta vacia, y esta lista para agregar productos");
-            } else {
-                service.eliminarTodosLosProductos(itDb.getId());
-                return ResponseEntity.ok("Tu lista se ha reestablecido de nuevo, para la siguiente compra");
-            }
-        } else {
-            throw new NotFoundException("El item producto no se ha encontrado para realizar la compra nuevamente");
-        }
-    }
-
-    /* Lógica de negocio para los productos de ItemProducto */
-    @PostMapping("/agregar-producto/{itemProductoId}/{productoId}/{cantidad}")
-    public ResponseEntity<?> agregarProducto(
-            @PathVariable Long itemProductoId,
-            @PathVariable Long productoId,
-            @PathVariable int cantidad) {
-        service.agregarProducto(itemProductoId, productoId, cantidad);
-        return ResponseEntity.ok("Producto agregado al ItemProducto");
-    }
-
-    @PutMapping("/modificar-estado-pedido/{itemProductoId}")
-    public ResponseEntity<?> modificarPedido(
-            @PathVariable Long itemProductoId) {
-        ItemProducto itemProductoDb = service.findById(itemProductoId);
-        service.updateEstadoPedido(itemProductoDb);
-        return ResponseEntity.ok("El estado del pedido ha sido modificado");
-    }
-
-    @DeleteMapping("/eliminar-producto/{itemProductoId}/{productoId}")
-    public ResponseEntity<?> eliminarProducto(
-            @PathVariable Long itemProductoId,
-            @PathVariable Long productoId) {
-        service.eliminarProducto(itemProductoId, productoId);
-        return ResponseEntity.ok("Producto eliminado del ItemProducto");
-    }
-
-    @DeleteMapping("/eliminar-todos-los-productos/{itemProductoId}")
-    public ResponseEntity<?> eliminarTodosLosProductos(@PathVariable Long itemProductoId) {
-        service.eliminarTodosLosProductos(itemProductoId);
-        return ResponseEntity.ok("Todos los productos han sido eliminados del ItemProducto");
+        return ResponseEntity.badRequest().body("El producto no se ha eliminado");
     }
 }
